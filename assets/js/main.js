4a005ad51151cc9c41d1b8c1a080be2239e00bb5
Kaique Garcia Menezes <contato@kaiquegarcia.dev>
2023-02-01 23:12:28 -0300

    feat: develop a functional preloading

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,44 @@
-const progressBar = document.querySelector('.progress-bar'),
-    input = document.querySelector('input[name="percentage"]');
+const body = document.body;
+const preloading = document.querySelector('.preloading'),
+    progressBar = document.querySelector('.preloading .progress-bar');
 
-input.addEventListener('change', function() {
-    const percentage = input.value;
-    progressBar.style.width = percentage + "%";
-})
+function closePreloading() {
+    preloading.style.opacity = '0';
+    setTimeout(function() {
+        preloading.style.display = 'none';
+        body.className = body.className.replace('disable-scroll', '');
+    }, 1200);
+}
+
+function setPreloadingProgress(percentage) {
+    progressBar.style.width = `${percentage}%`;
+
+    if (percentage === 100) {
+        closePreloading();
+    }
+}
+
+const elements = document.querySelectorAll('img');
+var totalLoaded = 0;
+
+function addLoaded() {
+    totalLoaded++;
+    let percentage = (totalLoaded/elements.length) * 100;
+    setPreloadingProgress(percentage);
+}
+
+for(let element of elements) {
+    if (element.complete && element.naturalHeight !== 0) {
+        addLoaded();
+        continue;
+    }
+
+    let previousOnload = element.onload;
+    element.onload = function() {
+        if ('function' === typeof previousOnload) {
+            previousOnload();
+        }
+
+        addLoaded();
+    }
+}
